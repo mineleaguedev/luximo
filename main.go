4446bc4189b9d407321139deba8be298c584fb63
Abdymazhit <islam.abdymazhit@gmail.com>
2022-03-25 22:38:18 +0300

    Add server start

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"github.com/fasthttp/router"
+	"github.com/valyala/fasthttp"
+	"log"
+)
 
+func main() {
+	r := router.New()
+	log.Fatal(fasthttp.ListenAndServe(":8080", r.Handler))
 }
