2ecb62fc4ce9525379b12ea5ddde7987e5056230
Abdymazhit <islam.abdymazhit@gmail.com>
2022-03-26 00:01:50 +0300

    Add config

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"github.com/mineleaguedev/luximo/handlers"
 	"github.com/mineleaguedev/luximo/services"
+	"github.com/spf13/viper"
 	"github.com/valyala/fasthttp"
 	"log"
 )
 
 func main() {
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config: %s", err.Error())
+	}
+
 	service := services.NewService()
 	handler := handlers.NewHandler(service)
 
